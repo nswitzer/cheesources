9dfa3a46f18cb84440c0f531da2ff0722998273a
Nick Switzer <nswitzer@elevatedthird.com>
2016-06-25 17:23:32 -0600

    adds navigation

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@
 
 	app.controller('rsrcFltr', function($scope) {
 
-	// Define list of documents
-	$scope.documents = [
+	// Define list of my documents
+	$scope.myDocuments = [
 	 	{
 	 		type:"file",
 	 		title:"File Upload 1"
@@ -43,6 +43,80 @@
 	 	}
 	];
 
+	// Define list of all documents
+	$scope.allDocuments = [
+	 	{
+	 		type:"file",
+	 		title:"File Upload 1"
+	 	},
+	 	{
+	 		type:"book",
+	 		title:"Book 1"
+	 	},
+	 	{
+	 		type:"podcast",
+	 		title:"Podcast 1"
+	 	},
+	 	{
+	 		type:"file",
+	 		title:"File Upload 2"
+	 	},
+	 	{
+	 		type:"book",
+	 		title:"Book 2"
+	 	},
+	 	{
+	 		type:"podcast",
+	 		title:"Podcast 2"
+	 	},{
+	 		type:"file",
+	 		title:"File Upload 3"
+	 	},
+	 	{
+	 		type:"book",
+	 		title:"Book 3"
+	 	},
+	 	{
+	 		type:"podcast",
+	 		title:"Podcast 3"
+	 	},
+	 	{
+	 		type:"file",
+	 		title:"File Upload 4"
+	 	},
+	 	{
+	 		type:"book",
+	 		title:"Book 4"
+	 	},
+	 	{
+	 		type:"podcast",
+	 		title:"Podcast 4"
+	 	},
+	 	{
+	 		type:"file",
+	 		title:"File Upload 5"
+	 	},
+	 	{
+	 		type:"book",
+	 		title:"Book 5"
+	 	},
+	 	{
+	 		type:"podcast",
+	 		title:"Podcast 5"
+	 	},{
+	 		type:"file",
+	 		title:"File Upload 6"
+	 	},
+	 	{
+	 		type:"book",
+	 		title:"Book 6"
+	 	},
+	 	{
+	 		type:"podcast",
+	 		title:"Podcast 6"
+	 	}
+	];
+
 	// Define filter list
 	$scope.docTypes = ["file","book","podcast"];
 
@@ -59,4 +133,5 @@
 	$scope.searchDocs = '';
 
 	});
+
 })();
